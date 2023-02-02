ebfb3eb84d69c65af0c886299e6651e50725f33a
John Ramos <jramos5883@gmail.com>
2023-02-01 16:49:04 -0800

    Weather app is working well, need to finished styling.

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 let weather = {
-    apiKey :  "9b55d4af647732f3bd6ef206211780bf",
-    fetchWeather: function () {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=London&appid=9b55d4af647732f3bd6ef206211780bf&units=imperial"
-        ).then((response) => response.json())
-        .then((data) => console.log(data));
+  apiKey: "9b55d4af647732f3bd6ef206211780bf",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&appid=" +
+        this.apiKey +
+        "&units=imperial"
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + " °F";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + " %";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " mph";
+    document.body.style.backgroundImage = "url('http://source.unsplash.com/1920x1080/?" + name + "')"
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document
+  .querySelector(".search button")
+  .addEventListener("click", function () {
+    weather.search();
+  });
+
+  document.querySelector(".search-bar").addEventListener("keyup", function () {
+    if (event.key == "Enter") {
+        weather.search();
     }
-}
+  });
+
+  weather.fetchWeather("Los Angeles");
